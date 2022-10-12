b164a3e22300c672deabf8a04a4aa81eecfc1607
svalvastic <assogames2@gmail.com>
2022-10-12 16:26:25 +0200

    ✨ feat(php): other info

--- script.php
+++ script.php
@@ -1,6 +1,14 @@
 <?php
 $paragraph = "ciao, mi chiamo franco, questo è il mio primo codice in php. un classico HELLO WORLD";
 $query = $_GET['bad'];
-$paragraph = str_replace($query, "***", $paragraph);
-echo $paragraph
+$paragraphModified = str_replace($query, "***", $paragraph);
 ?>
+
+
+<h1><?= $paragraph ?></h1>
+<p> La stringa è originariamente lunga <?= strlen($paragraph) ?></p>
+
+<h2>La badWord scelta è "<?= $query ?>" </h2>
+
+<h3> La frase censurata è:</h3>
+<h3>   <?= $paragraphModified  ?></h3>
